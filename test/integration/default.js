f487dc8035ea8337e3291e1e0b110aec4240d2c8
Pixels & Bytes <pxandbytes@gmail.com>
2013-11-16 14:36:38 -0800

    Add integration tests for other log levels

--- test/integration/default.js
+++ test/integration/default.js
@@ -4,8 +4,8 @@ require('should');
 var Logger = require('./../../lib/logger'),
     utils = require('./../utils');
 
-function logMe(logger, level, msg) {
-    logger[level](msg);
+function logMe(log, level, msg) {
+    log[level](msg);
 }
 
 /* global describe, before, it */
@@ -27,6 +27,78 @@ describe('Default logger', function() {
             } else {
                 logNames.length.should.equal(0);
             }
+        });
+
+    });
+
+    describe('Logging an info message', function() {
+        var message = 'Chocolate is sweet';
+
+        before(function() {
+            logMe(logger, 'info', message);
+        });
+
+        it('should add info log message to log file', function() {
+            var logNames = utils.getLogNames();
+            logNames.length.should.be.above(0);
+
+            var lastLogLine = utils.readLastLogLine();
+            lastLogLine.should.endWith('[INFO] [master] logMe - ' + message);
+            
+        });
+
+    });
+
+    describe('Logging a warning message', function() {
+        var message = 'Chocolate with >80% cocoa can be bitter';
+
+        before(function() {
+            logMe(logger, 'warn', message);
+        });
+
+        it('should add warning log message to log file', function() {
+            var logNames = utils.getLogNames();
+            logNames.length.should.be.above(0);
+
+            var lastLogLine = utils.readLastLogLine();
+            lastLogLine.should.endWith('[WARN] [master] logMe - ' + message);
+            
+        });
+
+    });
+
+    describe('Logging an error message', function() {
+        var message = 'Chocolate may make you fat';
+
+        before(function() {
+            logMe(logger, 'error', message);
+        });
+
+        it('should add error log message to log file', function() {
+            var logNames = utils.getLogNames();
+            logNames.length.should.be.above(0);
+
+            var lastLogLine = utils.readLastLogLine();
+            lastLogLine.should.endWith('[ERROR] [master] logMe at ' + __filename + ':8 - ' + message);
+            
+        });
+
+    });
+
+    describe('Logging a fatal message', function() {
+        var message = 'Chocolate can go rancid after a while';
+
+        before(function() {
+            logMe(logger, 'fatal', message);
+        });
+
+        it('should add fatal log message to log file', function() {
+            var logNames = utils.getLogNames();
+            logNames.length.should.be.above(0);
+
+            var lastLogLine = utils.readLastLogLine();
+            lastLogLine.should.endWith('[FATAL] [master] logMe at ' + __filename + ':8 - ' + message);
+            
         });
 
     });
