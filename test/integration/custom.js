1450eccafda67697ceded968b8d045dabd489b7c
Pixels & Bytes <pxandbytes@gmail.com>
2013-11-23 16:01:50 -0800

    Fix logger for clusters. Separated default and custom logger tests into separate files.

--- test/integration/custom.js
+++ test/integration/custom.js
@@ -9,26 +9,32 @@ function logMe(log, level, msg) {
     log[level](msg);
 }
 
-/* global describe, before, it */
-describe('Default logger', function() {
+/* global describe, before, beforeEach, it */
+describe('Custom logger', function() {
     var Logger = require('./../../lib/logger');
+    var logsDir = 'logs-problems';
     var logger = Logger.makeInst();
 
-    before(function() {
-        Logger.configure();
+    beforeEach(function() {
+        var config = {
+            level: 'WARN',
+            fileName: logsDir + '/app'
+        };
+        Logger.configure(config);
     });
 
     describe('Logging a debug message', function() {
-        var message = 'Life is like a box of chocolates';
+        /* jshint quotmark: false */
+        var message = "I'm singing in the rain";
 
         before(function() {
             logMe(logger, 'debug', message);
         });
 
         it('should not add debug log message to log file', function() {
-            var logNames = utils.getLogNames();
+            var logNames = utils.getLogNames(logsDir);
             if (logNames.length > 0) {
-                var lastLogLine = utils.readLastLogLine();
+                var lastLogLine = utils.readLastLogLine(logsDir);
                 lastLogLine.should.not.endWith('[DEBUG] [master] logMe - ' + message);
             } else {
                 logNames.length.should.equal(0);
@@ -38,28 +44,26 @@ describe('Default logger', function() {
     });
 
     describe('Logging an info message', function() {
-        var message = 'Chocolate is sweet';
+        var message = 'The rain started at 3 PM';
 
         before(function() {
             logMe(logger, 'info', message);
         });
 
-        it('should add info log message to log file', function(done) {
-            setTimeout(function() {
-                var logNames = utils.getLogNames();
-                logNames.length.should.be.above(0);
-
-                var lastLogLine = utils.readLastLogLine();
-                lastLogLine.should.endWith('[INFO] [master] logMe - ' + message);
-
-                done();
-            }, assertDelay);
+        it('should not add info log message to log file', function() {
+            var logNames = utils.getLogNames(logsDir);
+            if (logNames.length > 0) {
+                var lastLogLine = utils.readLastLogLine(logsDir);
+                lastLogLine.should.not.endWith('[INFO] [master] logMe - ' + message);
+            } else {
+                logNames.length.should.equal(0);
+            }
         });
 
     });
 
     describe('Logging a warning message', function() {
-        var message = 'Chocolate with >80% cocoa can be bitter';
+        var message = 'Thunderstorms forecasted for 5 PM';
 
         before(function() {
             logMe(logger, 'warn', message);
@@ -67,10 +71,10 @@ describe('Default logger', function() {
 
         it('should add warning log message to log file', function(done) {
             setTimeout(function() {
-                var logNames = utils.getLogNames();
+                var logNames = utils.getLogNames(logsDir);
                 logNames.length.should.be.above(0);
 
-                var lastLogLine = utils.readLastLogLine();
+                var lastLogLine = utils.readLastLogLine(logsDir);
                 lastLogLine.should.endWith('[WARN] [master] logMe - ' + message);
 
                 done();
@@ -80,7 +84,7 @@ describe('Default logger', function() {
     });
 
     describe('Logging an error message', function() {
-        var message = 'Chocolate may make you fat';
+        var message = 'Severe thunderstorms in your area';
 
         before(function() {
             logMe(logger, 'error', message);
@@ -88,10 +92,10 @@ describe('Default logger', function() {
 
         it('should add error log message to log file', function(done) {
             setTimeout(function() {
-                var logNames = utils.getLogNames();
+                var logNames = utils.getLogNames(logsDir);
                 logNames.length.should.be.above(0);
 
-                var lastLogLine = utils.readLastLogLine();
+                var lastLogLine = utils.readLastLogLine(logsDir);
                 lastLogLine.should.endWith('[ERROR] [master] logMe at ' + __filename + ':9 - ' + message);
 
                 done();
@@ -101,7 +105,7 @@ describe('Default logger', function() {
     });
 
     describe('Logging a fatal message', function() {
-        var message = 'Chocolate can go rancid after a while';
+        var message = 'ALERT: Tornado outbreak';
 
         before(function() {
             logMe(logger, 'fatal', message);
@@ -109,10 +113,10 @@ describe('Default logger', function() {
 
         it('should add fatal log message to log file', function(done) {
             setTimeout(function() {
-                var logNames = utils.getLogNames();
+                var logNames = utils.getLogNames(logsDir);
                 logNames.length.should.be.above(0);
 
-                var lastLogLine = utils.readLastLogLine();
+                var lastLogLine = utils.readLastLogLine(logsDir);
                 lastLogLine.should.endWith('[FATAL] [master] logMe at ' + __filename + ':9 - ' + message);
 
                 done();
@@ -120,5 +124,5 @@ describe('Default logger', function() {
         });
 
     });
- 
+
 });
